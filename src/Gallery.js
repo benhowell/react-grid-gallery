52739c94f4a43ae299c1f767ed9454eab4affd1b
ben <ben@psychstudio.com>
2016-08-21 11:04:06 +1000

    tidy

--- src/Gallery.js
+++ src/Gallery.js
@@ -1,5 +1,5 @@
 import React, { Component, PropTypes } from 'react';
-//import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom';
 import Lightbox from 'react-images';
 import Image from './Image.js';
 
