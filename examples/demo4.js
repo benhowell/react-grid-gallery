6f3ba72f1ab1fcd03fa15f891101a8d254fcb154
ben <ben@psychstudio.com>
2017-04-22 14:51:35 +1000

    new demo for customOverlay

--- examples/demo4.js
+++ examples/demo4.js
@@ -4,126 +4,150 @@ import Gallery from '../src/Gallery';
 
 
 class Demo4 extends React.Component {
-	constructor(props){
-		super(props);
+    constructor(props){
+        super(props);
+        this.state = {
+            images: this.props.images
+        };
+    }
 
-		this.state = {
-			images: this.props.images
-		};
+    render () {
+        var captionStyle = {
+            backgroundColor: 'rgba(0, 0, 0, 0.8)',
+            maxHeight: '240px',
+            overflow: 'auto',
+            position: 'absolute',
+            bottom: '0',
+            width: '100%',
+            color: 'white',
+            padding: '3px',
+            fontSize: '0.8em'
+        };
 
-	}
+        var chipStyle = {
+            wordWrap: 'break-word',
+            display: 'inline-block',
+            backgroundColor: '#fff',
+            height: 'auto',
+            lineHeight: 'inherit',
+            padding: '2px',
+            borderRadius: '2px',
+            color: 'black',
+            margin: '2px'
+        };
 
-	render () {
-		var captionStyle = {
-			backgroundColor: 'rgba(0, 0, 0, 0.8)',
-			maxHeight: '240px',
-			overflow: 'auto',
-			position: 'absolute',
-			bottom: '0',
-			width: '100%',
-			color: 'white',
-			padding: '3px',
-			fontSize: '0.8em'
-		};
-
-		var imageTitleStyle = {
-			fontSize: '1.5em'
-		};
-
-		var chipStyle = {
-			display: 'inline-block',
-			backgroundColor: '#fff',
-			height: 'auto',
-			lineHeight: 'inherit',
-			padding: '2px 2px',
-			borderRadius: '2px',
-			color: 'black',
-			marginRight: '3px'
-		};
-
-		var images = this.state.images.map(i => {
-			i.customOverlay = (
-				<div style={captionStyle}>
-					<div style={imageTitleStyle}>
-						<span>{i.name}</span>
-					</div>
-					<div>
-						by: {i.artist}
-					</div>
-					<p>{i.caption}</p>
-					<div className="tags">
-						{i.tags ? i.tags.map(t => {
-							return (<div key={t.value} style={chipStyle}>{t.title}</div>);
-						}) : ''}
-					</div>
-				</div>
-			);
-			i.tags = [];
-			return i;
-		});
-
-		return (
-			<div style={{
-				display: "block",
-				minHeight: "1px",
-				width: "100%",
-				border: "1px solid #ddd",
-				overflow: "auto"}}>
-				<Gallery
-					images={images}
-					rowHeight={240}
-					enableImageSelection={false}/>
-			</div>
-		);
-	}
+        var images = this.state.images.map((i) => {
+            i.customOverlay = (
+                    <div style={captionStyle}>
+                    <div>{i.caption}</div>
+                    {i.tags ? i.tags.map((t) => {
+                        return (
+                                <div key={t.value}
+                            style={chipStyle}>{t.title}</div>);}) : ''}
+                    </div>
+            );
+            i.tags = [];
+            return i;
+        });
+        return (
+                <div style={{
+                    display: "block",
+                    minHeight: "1px",
+                    width: "100%",
+                    border: "1px solid #ddd",
+                    overflow: "auto"}}>
+                <Gallery
+            images={images}
+            enableImageSelection={false}/>
+                </div>
+        );
+    }
 }
 
+
 Demo4.propTypes = {
-	images: React.PropTypes.arrayOf(
-		React.PropTypes.shape({
-			src: React.PropTypes.string.isRequired,
-			thumbnail: React.PropTypes.string.isRequired,
-			srcset: React.PropTypes.array,
-			caption: React.PropTypes.string,
-			thumbnailWidth: React.PropTypes.number.isRequired,
-			thumbnailHeight: React.PropTypes.number.isRequired
-		})
-	).isRequired
+    images: React.PropTypes.arrayOf(
+        React.PropTypes.shape({
+            src: React.PropTypes.string.isRequired,
+            thumbnail: React.PropTypes.string.isRequired,
+            srcset: React.PropTypes.array,
+            caption: React.PropTypes.string,
+            thumbnailWidth: React.PropTypes.number.isRequired,
+            thumbnailHeight: React.PropTypes.number.isRequired
+        })
+    ).isRequired
 };
 
+
 Demo4.defaultProps = {
-	images: shuffleArray([
-		{
-			src: "https://c2.staticflickr.com/9/8817/28973449265_07e3aa5d2e_b.jpg",
-			thumbnail: "https://c2.staticflickr.com/9/8817/28973449265_07e3aa5d2e_n.jpg",
-			thumbnailWidth: 320,
-			thumbnailHeight: 174,
-			tags: [{value: "Nature", title: "Nature"}, {value: "Flora", title: "Flora"}],
-			name: 'After Rain',
-			caption: "After Rain (Jeshu John - designerspics.com)",
-			artist: 'Jeshu John'
-		},
-		{
-			src: "https://c5.staticflickr.com/9/8768/28941110956_b05ab588c1_b.jpg",
-			thumbnail: "https://c5.staticflickr.com/9/8768/28941110956_b05ab588c1_n.jpg",
-			thumbnailWidth: 240,
-			thumbnailHeight: 320,
-			tags: [{value: "Nature", title: "Nature"}, {value: "Water", title: "Water"}],
-			name: '8H',
-			caption: "8H (gratisography.com)",
-			artist: 'gratisography.com'
-		},
-		{
-			src: "https://c7.staticflickr.com/9/8569/28941134686_d57273d933_b.jpg",
-			thumbnail: "https://c7.staticflickr.com/9/8569/28941134686_d57273d933_n.jpg",
-			thumbnailWidth: 320,
-			thumbnailHeight: 148,
-			tags: [{value: "People", title: "People"}],
-			name: '315H',
-			caption: "315H (gratisography.com)",
-			artist: 'gratisography.com'
-		}
-	])
+    images: shuffleArray([
+        {
+            src: "https://c7.staticflickr.com/9/8106/28941228886_86d1450016_b.jpg",
+            thumbnail: "https://c7.staticflickr.com/9/8106/28941228886_86d1450016_n.jpg",
+            thumbnailWidth: 271,
+            thumbnailHeight: 320,
+            tags: [{value: "Nature", title: "Nature | Flowers"}],
+            caption: "Orange Macro (Tom Eversley - isorepublic.com)"
+        },
+        {
+            src: "https://c3.staticflickr.com/9/8583/28354353794_9f2d08d8c0_b.jpg",
+            thumbnail: "https://c3.staticflickr.com/9/8583/28354353794_9f2d08d8c0_n.jpg",
+            thumbnailWidth: 320,
+            thumbnailHeight: 190,
+            tags: [{value: "Architecture", title: "Architecture | Outdoors"},
+                   {value: "Industrial", title: "Industrial"}],
+            caption: "286H (gratisography.com)"
+        },
+        {
+            src: "https://c7.staticflickr.com/9/8569/28941134686_d57273d933_b.jpg",
+            thumbnail: "https://c7.staticflickr.com/9/8569/28941134686_d57273d933_n.jpg",
+            thumbnailWidth: 320,
+            thumbnailHeight: 148,
+            tags: [{value: "People", title: "People"},
+                   {value: "Architecture", title: "Architecture | Outdoors"},
+                   {value: "Industrial", title: "Industrial"}],
+            caption: "315H (gratisography.com)"
+        },
+        {
+            src: "https://c6.staticflickr.com/9/8342/28897193381_800db6419e_b.jpg",
+            thumbnail: "https://c6.staticflickr.com/9/8342/28897193381_800db6419e_n.jpg",
+            thumbnailWidth: 320,
+            thumbnailHeight: 213,
+            caption: "201H (gratisography.com)"
+        },
+        {
+            src: "https://c2.staticflickr.com/9/8239/28897202241_1497bec71a_b.jpg",
+            thumbnail: "https://c2.staticflickr.com/9/8239/28897202241_1497bec71a_n.jpg",
+            thumbnailWidth: 248,
+            thumbnailHeight: 320,
+            caption: "Big Ben (Tom Eversley - isorepublic.com)"
+        },
+        {
+            src: "https://c1.staticflickr.com/9/8785/28687743710_870813dfde_h.jpg",
+            thumbnail: "https://c1.staticflickr.com/9/8785/28687743710_3580fcb5f0_n.jpg",
+            thumbnailWidth: 320,
+            thumbnailHeight: 113,
+            tags: [{value: "People", title: "People"},
+                   {value: "Industrial", title: "Industrial"}],
+            caption: "Red Zone - Paris (Tom Eversley - isorepublic.com)"
+        },
+        {
+            src: "https://c6.staticflickr.com/9/8520/28357073053_cafcb3da6f_b.jpg",
+            thumbnail: "https://c6.staticflickr.com/9/8520/28357073053_cafcb3da6f_n.jpg",
+            thumbnailWidth: 313,
+            thumbnailHeight: 320,
+            caption: "Wood Glass (Tom Eversley - isorepublic.com)"
+        },
+        {
+            src: "https://c8.staticflickr.com/9/8104/28973555735_ae7c208970_b.jpg",
+            thumbnail: "https://c8.staticflickr.com/9/8104/28973555735_ae7c208970_n.jpg",
+            thumbnailWidth: 320,
+            thumbnailHeight: 213,
+            tags: [{value: "Nature", title: "Nature | Flowers"}],
+            caption: "Flower Interior Macro (Tom Eversley - isorepublic.com)"
+        }
+    ])
 };
+
 
 ReactDOM.render(<Demo4 />, document.getElementById('demo4'));
