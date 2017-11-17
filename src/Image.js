f496bc8675737a90844589fec03f6a397bdfa176
Steve Robinson <steve.rob@me.com>
2017-11-17 08:51:11 +0530

    fixed oncontextmenu handler attr name

--- src/Image.js
+++ src/Image.js
@@ -187,7 +187,7 @@ class Image extends Component {
                 <img
             key={"img-"+this.props.index}
             src={this.props.item.thumbnail} title={this.props.item.caption}
-            style={this.thumbnailStyle()} oncontextmenu={this.props.disableContextMenu ? 'return false' : null} />
+            style={this.thumbnailStyle()} onContextMenu={this.props.disableContextMenu ? 'return false' : null} />
                 </div>
                 {this.props.item.thumbnailCaption && (
                         <div className="tile-description"
